673adca5e80913d5a5fb424a2befa47efb4e3396
嘉佳 <frank.yang@cedarpark.org>
2021-12-23 18:09:52 -0800

    init
    added some basic testing code

---
+++ fwylib/src/Util.java
@@ -0,0 +1,13 @@
+public class Util {
+    public static String getStringOne() {
+        return "One";
+    }
+
+    public static int getIntTwo() {
+        return 2;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
